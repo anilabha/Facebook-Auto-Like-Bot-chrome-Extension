90171fd6d8019dd1d02fdd9d25bc3a4147cf3322
Anilabha Baral <40207220+anilabha@users.noreply.github.com>
2020-01-15 23:30:22 +0530

    Facebook-Auto-Like-Bot-chrome-Extension

---
+++ Facebook_auto_like_bot/facebook.js
@@ -0,0 +1,27 @@
+setInterval(() => {
+    let c = 0;
+    for (i = 0; i <= 10; i++) {
+        document.querySelector('i.sx_44a25c').click();
+        c++
+        console.log(c + " likes");
+
+    }
+}, 2000);
+
+
+
+
+
+// $(function() {
+//     $('#abc').click(function() {
+
+//         setInterval(() => {
+//             for (i = 0; i <= 10; i++) {
+//                 document.querySelector('i.sx_44a25c').click();
+
+//             }
+//         }, 1000);
+
+
+//     })
+// })
